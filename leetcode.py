a783aea7b94b8d3ce8e9c9644d8ecaf40a337cfd
Izma Mujeeb <izma.mujeeb@gmail.com>
2024-09-28 21:06:02 -0400

    added search question

--- leetcode.py
+++ leetcode.py
@@ -77,3 +77,15 @@ def isValid(s):
 
 def isAnagram(s, t):
     return sorted(s) == sorted(t) 
+
+def search(nums, target):
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1 
